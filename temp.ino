e3e92a06976bd1e251f4e004c8d10e445b947a99
bst0811 <40161599+bst0811@users.noreply.github.com>
2020-09-28 13:32:25 +0300

    added code for the fan with temperature

--- temp.ino
+++ temp.ino
@@ -1,21 +1,48 @@
-int val;
-int tempPin =1;
-//C:\Users\VICTOR SHININGSTAR\AppData\Local\Temp\arduino_build_366061\temp.ino.hex
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+// Data wire is plugged into digital pin 2 on the Arduino
+#define ONE_WIRE_BUS 2
+int fan = 8;
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);  
+
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  sensors.begin();  // Start up the library
+  Serial.begin(9600);
+  analogWrite(fan, LOW);
+ pinMode(fan,OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  val = analogRead(tempPin);
-  float mv = ( val/1024.0)*5000;
-  float cel = mv/10;
+void loop(void)
+{ 
+  // Send the command to get temperatures
+  sensors.requestTemperatures(); 
 
-  Serial.print("TEMPERATURE = ");
-  Serial.print(cel);
-   Serial.print("*C");
-   Serial.println();
-   delay(1000);
+  //print the temperature in Celsius
+  int c = sensors.getTempCByIndex(0);
+  Serial.print("Temperature: ");
+  Serial.print(c);
+  Serial.print((char)223);//shows degrees character
+  Serial.print("C  |  ");
+  
+  //print the temperature in Fahrenheit
+  Serial.print((sensors.getTempCByIndex(0) * 9.0) / 5.0 + 32.0);
+  Serial.print((char)176);//shows degrees character
+  Serial.println("F");
+
+  if(c<28){
+    analogWrite(fan, HIGH);
+    delay(2000);
+    }
+  else
+  {
+    analogWrite(fan, LOW);
+    delay(2000);
+    }
+  delay(2000);
 }
