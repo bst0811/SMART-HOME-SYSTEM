46fea8ef8ca1cf994314f5dbd6c42833726ef4e8
WaiswaV <31123444+victorwb@users.noreply.github.com>
2020-12-17 19:55:33 +0300

    Update MainActivity2.java
    adjusted pin numbers to be controlled based on embedded system

--- MainActivity2.java
+++ MainActivity2.java
@@ -10,6 +10,28 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.view.MotionEvent;
+
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
 /*imports for wifi*/
 
 import com.example.Status;
@@ -20,8 +42,13 @@ import java.nio.charset.Charset;
 
 
 public class MainActivity2 extends AppCompatActivity {
-    Switch light,door,fan,curtain;
+    Switch light,door,fan,alarm;
     Button btn_status;
+    public static final Integer RecordAudioRequestCode = 1;
+    private SpeechRecognizer speechRecognizer;
+    private EditText editText;
+    private ImageView micButton;
+
 
     Socket echoSocket = null;
     DataOutputStream os = null;
@@ -32,10 +59,67 @@ public class MainActivity2 extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    echoSocket = new Socket("192.168.4.1", 80);
+                    //echoSocket = new Socket("192.168.4.1", 80);
+                   echoSocket = new Socket("192.168.43.30", 80);
                     os = new DataOutputStream(echoSocket.getOutputStream());
                     is = new DataInputStream(echoSocket.getInputStream());
-                    os.write("pin=4".getBytes(Charset.forName("UTF-8")));
+                    os.write("pin=9".getBytes(Charset.forName("UTF-8")));
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity2.this, e.toString(),Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+
+    void DoorControl(){
+        final Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    //echoSocket = new Socket("192.168.4.1", 80);
+                    echoSocket = new Socket("192.168.43.30", 80);
+                    os = new DataOutputStream(echoSocket.getOutputStream());
+                    is = new DataInputStream(echoSocket.getInputStream());
+                    os.write("pin=6".getBytes(Charset.forName("UTF-8")));
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity2.this, e.toString(),Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+    void FanControl(){
+        final Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    //echoSocket = new Socket("192.168.4.1", 80);
+
+                    echoSocket = new Socket("192.168.43.30", 80);
+                    os = new DataOutputStream(echoSocket.getOutputStream());
+                    is = new DataInputStream(echoSocket.getInputStream());
+                    os.write("pin=5".getBytes(Charset.forName("UTF-8")));
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity2.this, e.toString(),Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+    void alarmControl(){
+        final Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    //echoSocket = new Socket("192.168.4.1", 80);
+                    echoSocket = new Socket("192.168.43.30", 80);
+                    os = new DataOutputStream(echoSocket.getOutputStream());
+                    is = new DataInputStream(echoSocket.getInputStream());
+                    os.write("pin=8".getBytes(Charset.forName("UTF-8")));
                 } catch (Exception e) {
                     Toast.makeText(MainActivity2.this, e.toString(),Toast.LENGTH_SHORT).show();
                     e.printStackTrace();
@@ -49,11 +133,18 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            checkPermission();
+        }
+
+        editText = findViewById(R.id.text);
+        micButton = findViewById(R.id.button);
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
 
         light = findViewById(R.id.switch1);
         door = findViewById(R.id.switch3);
         fan = findViewById(R.id.switch2);
-        curtain = findViewById(R.id.switch4);
+        alarm = findViewById(R.id.switch4);
         btn_status = findViewById(R.id.btn_status);
 
 
@@ -80,7 +171,14 @@ public class MainActivity2 extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
                     Toast.makeText(MainActivity2.this, "Door is open",Toast.LENGTH_SHORT).show();
+                    try {
+                        DoorControl();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity2.this, e.toString(),Toast.LENGTH_SHORT).show();
+                    }
                 }else{
+                    DoorControl();
                     Toast.makeText(MainActivity2.this, "Door is closed",Toast.LENGTH_SHORT).show();
                 }
             }
@@ -91,19 +189,33 @@ public class MainActivity2 extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
                     Toast.makeText(MainActivity2.this, "fan is on",Toast.LENGTH_SHORT).show();
+                    try {
+                        FanControl();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity2.this, e.toString(),Toast.LENGTH_SHORT).show();
+                    }
                 }else{
+                    FanControl();
                     Toast.makeText(MainActivity2.this, "fan is off",Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        curtain.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        alarm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
-                    Toast.makeText(MainActivity2.this, "Curtain is drawn",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity2.this, "Alarm is on",Toast.LENGTH_SHORT).show();
+                    try {
+                        alarmControl();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity2.this, e.toString(),Toast.LENGTH_SHORT).show();
+                    }
                 }else{
-                    Toast.makeText(MainActivity2.this, "Curtain is closed",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity2.this, "Alarm is off",Toast.LENGTH_SHORT).show();
+                    alarmControl();
                 }
             }
         });
@@ -115,6 +227,130 @@ public class MainActivity2 extends AppCompatActivity {
 
             }
         });
+
+
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                editText.setText("");
+                editText.setHint("Listening...");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                micButton.setImageResource(R.drawable.mic);
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if( data.get(0).equals("door open")){
+                    editText.setText(data.get(0));
+                door.toggle();
+                }else if(data.get(0).equals("door close")){
+                    editText.setText(data.get(0));
+                    door.toggle();
+                }else if(data.get(0).equals("light on")){
+                    editText.setText(data.get(0));
+                    light.toggle();
+                }
+                else if(data.get(0).equals("light off")){
+                    editText.setText(data.get(0));
+                    light.toggle();
+                }
+                else if(data.get(0).equals("fan on")){
+                    editText.setText(data.get(0));
+                    fan.toggle();
+                }else if(data.get(0).equals("fan off")){
+                    editText.setText(data.get(0));
+                    fan.toggle();
+                }
+                else if(data.get(0).equals("alarm on")){
+                    editText.setText(data.get(0));
+                    alarm.toggle();
+                }
+                else if(data.get(0).equals("alarm off")){
+                    editText.setText(data.get(0));
+                    alarm.toggle();
+                }
+                else{
+                    editText.setText("wrong command! Try these door open/close, light on/off, fan on/off, alarm on/off");
+                }
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+        micButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    speechRecognizer.stopListening();
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    micButton.setImageResource(R.drawable.mic);
+                    speechRecognizer.startListening(speechRecognizerIntent);
+                }
+                return false;
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        speechRecognizer.destroy();
+    }
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RecordAudioRequestCode && grantResults.length > 0 ){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
+        }
     }
 
 
